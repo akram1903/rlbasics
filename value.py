c9231de0c2e9de07d5567027dfdf6e59ae86db12
SaifSeddik <saifr5092@gmail.com>
2023-12-31 02:40:33 +0200

    value finish

--- value.py
+++ value.py
@@ -7,6 +7,7 @@ class MazeSolver:
         self.barrier_prob = barrier_prob
         self.maze = np.zeros((size, size))  # 0 represents an empty cell
         self.generate_maze()
+        terminal_reward = 1
 
         # Start state at top-left corner
         self.start_state = (0, 0)
@@ -14,17 +15,18 @@ class MazeSolver:
         self.terminal_state = (size - 1, size - 1)
 
     def generate_maze(self):
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(self.size-1):
+            for j in range(self.size-1):
                 if np.random.rand() < self.barrier_prob:
                     self.maze[i, j] = 1  # 1 represents a barrier
-                    self.maze[self.size-1, self.size-1] = 0  # 1 represents a barrier
+                    # self.maze[self.size-1, self.size-1] = 0  #to make sure that the termnal state is not barrier
 
 
     def is_valid_move(self, x, y):
         return 0 <= x < self.size and 0 <= y < self.size and self.maze[x, y] == 0
 
-    def value_iteration(self, discount_factor=0.9, theta=0.4, max_iterations=1000):
+    def value_iteration(self, discount_factor=0.9, theta=0.3, max_iterations=1000):
+        # The value function is initialized to zeros for all states.
         value_function = np.zeros((self.size, self.size))
         for iteration in range(max_iterations):
             print(f"\nValue Iteration - Iteration {iteration + 1}:\n")
@@ -32,18 +34,18 @@ class MazeSolver:
             delta = 0
             for i in range(self.size):
                 for j in range(self.size):
+                    # if barier skip
                     if self.maze[i, j] == 1:
                         continue
                     v = value_function[i, j]
                     # if (i, j) == self.terminal_state:
                     #     # Value of terminal state is 0
                     #     value_function[i, j] = 0
-                    if True:
-                        value_function[i, j] = self.calculate_max_value(i, j, value_function, discount_factor)
+                    # Value Iteration Updates based on 1 step lookahed
+                    value_function[i, j] = self.calculate_max_value(i, j, value_function, discount_factor)
                         # v[self.size,self.size] = value_function[i,j]+1
-
                     delta = max(delta, abs(v - value_function[i, j]))
-
+            
             self.print_maze(value_function)
 
             if delta < theta:
@@ -51,13 +53,25 @@ class MazeSolver:
 
     def calculate_max_value(self, x, y, value_function, discount_factor):
         max_value = float('-inf')
+        # get the max value from the 4 actions
         for action in range(4):
             next_x, next_y = self.get_next_position(x, y, action)
             if self.is_valid_move(next_x, next_y):
-                max_value = max(max_value, 1 + discount_factor * value_function[next_x, next_y])
+                # The formula for updating the value function is consistent
+                # with the one-step lookahead approach. It considers the reward 
+                # for the current state-action pair plus the discounted expected value of the next state.
+                if next_x == self.size-1 and next_y == self.size-1:
+                    reward = 1 
+                elif x == self.size-1 and y == self.size-1:
+                    reward = 1
+                else:
+                    reward = 0
+
+                max_value = max(max_value,reward + discount_factor * value_function[next_x, next_y])
 
         return max_value
 
+        
     def get_next_position(self, x, y, action):
         if action == 0:  # Up
             return x - 1, y
@@ -82,7 +96,7 @@ class MazeSolver:
             print()
 
 if __name__ == "__main__":
-    size = 4
+    size = 7
     barrier_prob = 0.2
     maze_solver = MazeSolver(size, barrier_prob)
 
@@ -91,3 +105,4 @@ if __name__ == "__main__":
 
     print("\nValue Iteration:")
     maze_solver.value_iteration()
+
